d45fdc6e8b1094e42132544319afc187f3fa54bd
Trikolon <1077860+Trikolon@users.noreply.github.com>
2018-03-08 14:04:42 +0100

    Configuration handling improvements  * Add default keys if they are missing in config file  * Separate method for configuration initialization  * Moved config documentation to header

--- src/main/java/com/tallcraft/chatreplay/ChatReplay.java
+++ src/main/java/com/tallcraft/chatreplay/ChatReplay.java
@@ -1,21 +1,24 @@
 package com.tallcraft.chatreplay;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.MemoryConfiguration;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bstats.bukkit.Metrics;
 
 import java.util.logging.Logger;
 
 public class ChatReplay extends JavaPlugin implements Listener {
     private static final Logger logger = Logger.getLogger("minecraft");
     private ChatBuffer chatBuffer;
+    private FileConfiguration config;
 
     @Override
     public void onEnable() {
@@ -23,8 +26,11 @@ public class ChatReplay extends JavaPlugin implements Listener {
         //Metrics powered by bstats.org
         Metrics metrics = new Metrics(this);
 
-        this.saveDefaultConfig();
 
+        // Set up configuration
+        initConfig(); //  Update config file if options are missing
+
+        // Load config options and initialize buffer
         configureBuffer();
 
         // Insert messages for testing
@@ -32,7 +38,48 @@ public class ChatReplay extends JavaPlugin implements Listener {
 //            chatBuffer.addMessage(new ChatMessage("Notch", Integer.toString(i + 1)));
 //        }
 
+        // Register events to listen for chat, command and player join
         getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    private void initConfig() {
+        // Get config object for "config.yml"
+        config = this.getConfig();
+
+        config.options().header("ChatReplay Configuration\n" +
+                "\n" +
+                " Chat Buffer Options:\n" +
+                "\n" +
+                "   bufferSize: How many of the most recent chat messages to store in total\n" +
+                "   viewSize: How many messages to show players at once. Vanilla Minecraft Client has a history limit of around 100\n" +
+                "\n" +
+                " Messages\n" +
+                "\n" +
+                "   Header and footer message of replay output\n" +
+                "   Available variables: {{msgCount}}\n" +
+                "     replayHeader\n" +
+                "     replayFooter\n" +
+                "\n" +
+                "   Formatting for single messages replayed\n" +
+                "   Available variables: {{player}}, {{message}}, {{timestamp}}\n" +
+                "     replayMsgFormat\n" +
+                "     replayMsgHover\n" +
+                "\n" +
+                "   Button to navigate chat history\n" +
+                "      navigateHistoryButtonText");
+
+        MemoryConfiguration defaultConfig = new MemoryConfiguration();
+        defaultConfig.set("bufferSize", 500);
+        defaultConfig.set("viewSize", 70);
+        defaultConfig.set("replayHeader", "&7&lReplaying last {{msgCount}} messages");
+        defaultConfig.set("replayFooter", "&7&lReplayed last {{msgCount}} messages");
+        defaultConfig.set("replayMsgFormat", "&7[{{player}}]: {{message}}");
+        defaultConfig.set("replayMsgHover", "{{timestamp}}");
+        defaultConfig.set("navigateHistoryButtonText", "&7&nSHOW OLDER");
+
+        config.setDefaults(defaultConfig);
+        config.options().copyDefaults(true);
+        saveConfig();
     }
 
 
@@ -42,7 +89,6 @@ public class ChatReplay extends JavaPlugin implements Listener {
     }
 
 
-    // TODO: permissions for sub-commands
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 
         if (args.length == 0) {
@@ -102,8 +148,8 @@ public class ChatReplay extends JavaPlugin implements Listener {
      * Update existing chatBuffer or create a new one with config options
      */
     private void configureBuffer() {
-        int bufferSize = getConfig().getInt("bufferSize");
-        int viewSize = getConfig().getInt("viewSize");
+        int bufferSize = config.getInt("bufferSize");
+        int viewSize = config.getInt("viewSize");
 
         if (bufferSize <= 0) {
             throw new IllegalArgumentException(this.getName() + ": Invalid bufferSize " + bufferSize + "! Must be greater 0");
@@ -113,11 +159,11 @@ public class ChatReplay extends JavaPlugin implements Listener {
             throw new IllegalArgumentException(this.getName() + ": Invalid viewSize. Can not be smaller than total buffer size");
         }
 
-        String replayHeader = getConfig().getString("replayHeader");
-        String replayFooter = getConfig().getString("replayFooter");
-        String replayMsgFormat = getConfig().getString("replayMsgFormat");
-        String replayMsgHover = getConfig().getString("replayMsgHover");
-        String navigateHistoryButtonText = getConfig().getString("navigateHistoryButtonText");
+        String replayHeader = config.getString("replayHeader");
+        String replayFooter = config.getString("replayFooter");
+        String replayMsgFormat = config.getString("replayMsgFormat");
+        String replayMsgHover = config.getString("replayMsgHover");
+        String navigateHistoryButtonText = config.getString("navigateHistoryButtonText");
 
         if (chatBuffer == null) {
             chatBuffer = new ChatBuffer(
